3160ebdecbdbc010c6b40e3c917fed998206981a
siddharth200119 <siddramnani@gmail.com>
2023-11-05 14:39:08 +0530

    finished graph analysis

--- Graph Analysis/graphAnalysis.py
+++ Graph Analysis/graphAnalysis.py
@@ -12,3 +12,13 @@ for node, edges in map.items():
 Graph = nx.Graph()
 
 Graph.add_edges_from(nodesAndEdges)
+
+node_list = Graph.nodes()
+edge_list = Graph.edges()
+
+print("number of nodes: " + str(len(node_list)) + " number of edges: " + str(len(edge_list)))
+
+#degree of nodes
+
+for node in node_list:
+    print("degree of node " + node + ": " + str(Graph.degree(node)))
